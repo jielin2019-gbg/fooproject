c09f452c2bff44898c2cd24930ac50fecec33335
Jie <wendy.linjie@gmail.com>
2020-02-11 12:08:47 +0100

    add xml files

---
+++ src/test/java/Test1.java
@@ -0,0 +1,10 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test1 {
+
+    @org.junit.jupiter.api.Test
+    void myMethod() {
+        assertEquals(true, Foo.myMethod());
+    }
+}
